0a269475907aee1421f7e56a05db763c720b5e17
ArtMoc <tem.mochalov@yandex.ru>
2022-02-08 21:57:48 +0300

    123

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -3,6 +3,10 @@
 
 //#define MESSAGE_BOX
 
+#define MY_BTN 1011
+
+CONST CHAR INVITATION[] = "Введите логин";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
@@ -16,22 +20,58 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 #endif // MESSAGE_BOX
 
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
-
 	return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	//hwnd - Handler to Window (окно)
+	//uMsg - сообщение
+	//wParam, lParam - параметры сообщения
 	switch (uMsg)
 	{
-	case WM_INITDIALOG:break;
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)INVITATION);
+	}
+	break;
 	case WM_COMMAND:
+	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK:MessageBox(NULL, "Была нажата кнопка Ок", "Info", MB_ICONINFORMATION);break;
-		case IDCANCEL:EndDialog(hwnd, 0);break;
+		case IDC_BTN_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);//получаем Handler текстового поля по номеру его ресурса
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		break;
+		case IDOK:MessageBox(NULL, "Была нажата кнопка Ок", "Info", MB_ICONINFORMATION);break;
+		case IDCANCEL:EndDialog(hwnd, 0);break;
+		case IDC_EDIT1:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)buffer);
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:
+				if (strcmp(buffer, INVITATION) == 0)
+					SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			case EN_KILLFOCUS:
+				//SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)buffer);
+				if (strcmp(buffer, "") == 0)SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)INVITATION);
+			}
+			break;
+		}
+		}
+		break;
+	}
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 	}
